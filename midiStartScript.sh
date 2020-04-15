7705e2fdccd83fa2c812bd9e727cb7fe01175a11
Jakki12 <jakkesprinter@web.de>
2020-04-15 19:57:57 +0200

    update

---
+++ midiStartScript.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+aconnect 'nanoPAD':0 'Pure Data':0
+aconnect 'Pure Data':1 'CME U2MIDI':0
---
+++ startScript.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo /usr/local/bin/pd -nogui -rt -open /home/pi/Documents/RSP/RSP/RSP_Main_Generic_Dynamic.pd
+
