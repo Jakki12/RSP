21eed70bae5686abd35d0febfaf13a2b7c671db7
Jakki12 <jakkesprinter@web.de>
2020-11-10 00:03:18 +0100

    script vergessen zu adden

--- startScript.sh
+++ startScript.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-#sudo /usr/local/bin/pd -nogui -rt -open /home/pi/Documents/RSP/RSP/RSP_Main_Generic_Dynamic.pd
+echo "starting startup script..."
+
+# start pd and load patch
+sudo /usr/local/bin/pd -nogui -rt -open /home/pi/Documents/RSP/RSP/RSP_Main_Generic_Dynamic.pd &
+
+
+# wait for pd to initialize
+KA=$(aconnect -l | grep "Pure Data")
+
+while [ "$KA" = "" ]
+do
+	sleep 1
+	echo "waiting for pd to init midi devices..."
+	KA=$(aconnect -l | grep "Pure Data")
+done
+echo "pd now should have midi devices running"
+
+# make midi connections
+echo "connecting Launchpad Midi-Out to Pure Data-In..."
+aconnect 'Launchpad Mini':0 'Pure Data':0
+
+echo "connecting Pure Data-Out to Launchpad Mini-In..."
+aconnect 'Pure Data':1 'Launchpad Mini':0
 
