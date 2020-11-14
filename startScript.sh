ebf105c89356274367ab4c233296865e3e7abb26
Jakki12 <jakkesprinter@web.de>
2020-11-14 21:51:58 +0100

    start on boot erfolgreich

--- startScript.sh
+++ startScript.sh
@@ -1,26 +1,32 @@
-#!/bin/bash
+ #!/bin/bash
 
 echo "starting startup script..."
 
 # start pd and load patch
-sudo /usr/local/bin/pd -nogui -rt -open /home/pi/Documents/RSP/RSP/RSP_Main_Generic_Dynamic.pd &
-
+sudo /usr/local/bin/pd -nogui -rt -blocksize 64 -audiobuf 80 -r 48000 -open /home/pi/Documents/RSP/RSP/RSP_Main_Generic_Dynamic.pd &
 
 # wait for pd to initialize
-KA=$(aconnect -l | grep "Pure Data")
+#KA=$(sudo aconnect -l | grep "Pure Data")
 
-while [ "$KA" = "" ]
-do
-	sleep 1
-	echo "waiting for pd to init midi devices..."
-	KA=$(aconnect -l | grep "Pure Data")
+#while [ "$KA" = "" ]
+#do
+#	sudo sleep 1
+#	sudo echo "waiting for pd to init midi devices..."
+#	KA=$(sudo aconnect -l | grep "Pure Data")
+#done
+
+while [ "$(sudo aconnect -l | grep "Pure Data")" = "" ];
+do 
+	sudo echo "fuck me"
+	sudo sleep 0.1
 done
-echo "pd now should have midi devices running"
+
+sudo echo "pd now should have midi devices running"
 
 # make midi connections
-echo "connecting Launchpad Midi-Out to Pure Data-In..."
-aconnect 'Launchpad Mini':0 'Pure Data':0
+sudo echo "connecting Launchpad Midi-Out to Pure Data-In..."
+sudo aconnect 'Launchpad Mini':0 'Pure Data':0
 
-echo "connecting Pure Data-Out to Launchpad Mini-In..."
-aconnect 'Pure Data':1 'Launchpad Mini':0
+sudo echo "connecting Pure Data-Out to Launchpad Mini-In..."
+sudo aconnect 'Pure Data':1 'Launchpad Mini':0
 
